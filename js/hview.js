da65b9634a9195835667e0274c0c4492ada77f4f
Stefan Andonian <andonian@google.com>
2022-06-28 21:19:49 -0700

    Highlight the last selected element in the hierarchy view's tree of nodes and border-box. Also update the selected, last selected, and hover colors to be different shades of blue.

--- js/hview.js
+++ js/hview.js
@@ -283,10 +283,11 @@ $(function () {
 
     const selectNode = function () {
         if ($(this).hasClass(CLS_SELECTED)) return;
-        $("#vlist_content .selected").removeClass(CLS_SELECTED);
+        $(".last_selected").removeClass(CLS_LAST_SELECTED);
+        $(".selected").removeClass(CLS_SELECTED).addClass(CLS_LAST_SELECTED);
         $(this).addClass(CLS_SELECTED);
 
-        $("#border-box .selected, #image-preview").removeClass(CLS_SELECTED).css('background-image', 'none');
+        $("#border-box .selected, #image-preview").css('background-image', 'none')
         const box = $(this).data("box").addClass(CLS_SELECTED);
 
         // Render properties;
