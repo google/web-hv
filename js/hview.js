6d367dcb563339bb9d92f452b51c48ec537205f8
Stefan Andonian <andonian@google.com>
2023-03-08 14:10:06 -0800

    Use the HTML element as the single source of truth, rather than trusting the information in the node, which often changes per frame for each element.

--- js/hview.js
+++ js/hview.js
@@ -535,7 +535,7 @@ $(function () {
         }
         menu.push(null);
 
-        if (!this.node.disablePreview) {
+        if (!this.node.el.classList.contains("preview-disabled")) {
             menu.push({
                 text: "Disable preview",
                 icon: "ic_hide",
@@ -883,7 +883,7 @@ $(function () {
         const heightFactor = currentRootNode.height / $(this).height();
 
         const updateSelection = function (node, x, y, firstNoDrawChild, clipX1, clipY1, clipX2, clipY2) {
-            if (node.disablePreview || !node.nodeDrawn || (nodesHidden && !node.isVisible)) {
+            if (node.el.classList.contains("preview-disabled") || !node.nodeDrawn || (nodesHidden && !node.isVisible)) {
                 return null;
             }
 
@@ -988,11 +988,9 @@ $(function () {
                 profileView(selectedNode);
                 break;
             case 3: // Disable preview
-                selectedNode.disablePreview = true;
                 selectedNode.el.classList.add("preview-disabled");
                 break;
             case 4: // Enable preview
-                selectedNode.disablePreview = false;
                 selectedNode.el.classList.remove("preview-disabled");
                 break;
             case 5: // Collapse all
